680349f036764cfa5045ed75f37c314e780d93f0
Greg Tracy <gtracy@gmail.com>
2022-02-13 13:57:32 -0600

    mop up duties.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express');
 
 
 const app = express();
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
